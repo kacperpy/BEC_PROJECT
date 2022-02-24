4181fa138afe7da2bb74d47f7bf25b0fec44c315
JanRejnowski <jrx@bec.dk>
2022-02-24 13:22:39 +0100

    added WebSecurityConfig to allow access to h2 database;

---
+++ src/main/java/dk/bec/bookanything/config/WebSecurityConfig.java
@@ -0,0 +1,16 @@
+package dk.bec.bookanything.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring()
+           .antMatchers("/h2-console/**");
+    }
+}
