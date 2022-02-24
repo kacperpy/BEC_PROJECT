2810a8681da9b7bff0b9cc751d135039e816ad12
kubagogolevski <jakub.gogolevski@gmail.com>
2022-02-24 15:16:50 +0100

    Models edited and added

--- src/main/java/dk/bec/bookanything/model/RoleModel.java
+++ src/main/java/dk/bec/bookanything/model/RoleModel.java
@@ -2,8 +2,10 @@ package dk.bec.bookanything.model;
 
 
 import lombok.Data;
+import org.apache.catalina.User;
 
 import javax.persistence.*;
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -21,4 +23,7 @@ public class RoleModel {
 
     @Column(length = 200)
     private String name;
+
+    @OneToMany
+    private List<UserModel> userList;
 }
