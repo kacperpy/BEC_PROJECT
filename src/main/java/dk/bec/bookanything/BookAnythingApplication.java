1bf8bccbbaf2b2ddd0eccea30be0a31fcee06593
Kacper Pyrzanowski <KPZ@bec.dk>
2022-02-24 10:46:38 +0100

    initial commit

---
+++ src/main/java/dk/bec/bookanything/BookAnythingApplication.java
@@ -0,0 +1,13 @@
+package dk.bec.bookanything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookAnythingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/bec/bookanything/BookAnythingApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.bec.bookanything;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAnythingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
