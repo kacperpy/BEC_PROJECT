d28faef85bc8f4f00bb42284cf54514370461393
kacperpy <kacperpy@gmail.com>
2022-02-25 10:44:59 +0100

    added bookable object repository annotation

--- src/main/java/dk/bec/bookanything/repository/springData/BookableObjectRepository.java
+++ src/main/java/dk/bec/bookanything/repository/springData/BookableObjectRepository.java
@@ -2,7 +2,9 @@ package dk.bec.bookanything.repository.springData;
 
 import dk.bec.bookanything.model.BookableObjectEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookableObjectRepository extends CrudRepository<BookableObjectEntity, Long> {
 
 }
