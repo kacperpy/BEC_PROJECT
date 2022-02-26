533b35a4a35329c07641172d24a250b257c6ec54
m4teoosh <mateusz.kadlubowski99@gmail.com>
2022-02-26 17:50:31 +0100

    completed CRUD for Address

--- src/main/java/dk/bec/bookanything/controller/AddressController.java
+++ src/main/java/dk/bec/bookanything/controller/AddressController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api")
@@ -18,29 +19,28 @@ public class AddressController {
         this.addressService = addressService;
     }
 
-    @GetMapping("/addresses/{id}")
-    ResponseEntity<AddressEntity> get(@PathVariable("id") Long id) {
-        Optional<AddressEntity> addressOptional = addressService.getAddressById(id);
+    @GetMapping("/addresses/{uuid}")
+    ResponseEntity<AddressEntity> getAddress(@PathVariable("uuid") UUID uuid) {
+        Optional<AddressEntity> addressOptional = addressService.getAddressByUuid(uuid);
         return addressOptional.map(addressEntity -> new ResponseEntity<>(addressEntity, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/addresses")
-    ResponseEntity<AddressEntity> add(@RequestBody AddressEntity address) {
+    ResponseEntity<AddressEntity> createAddress(@RequestBody AddressEntity address) {
         Optional<AddressEntity> addressOptional = addressService.createAddress(address);
         return addressOptional.map(addressEntity -> new ResponseEntity<>(addressEntity, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 
     @PutMapping("/addresses")
-    ResponseEntity<AddressEntity> update(@RequestBody AddressEntity address) {
+    ResponseEntity<AddressEntity> updateAddress(@RequestBody AddressEntity address) {
         Optional<AddressEntity> addressOptional = addressService.updateAddress(address);
         return addressOptional.map(addressEntity -> new ResponseEntity<>(addressEntity, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.BAD_REQUEST));
     }
 
-    @DeleteMapping("/addresses/{id}")
-    ResponseEntity<AddressEntity> delete(@PathVariable("id") Long id) {
-        addressService.deleteAddressById(id);
-        return addressService.getAddressById(id).isPresent() ? new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR) : new ResponseEntity<>(HttpStatus.OK);
+    @DeleteMapping("/addresses/{uuid}")
+    ResponseEntity<AddressEntity> deleteAddress(@PathVariable("uuid") UUID uuid) {
+        addressService.deleteAddressByUuid(uuid);
+        return addressService.getAddressByUuid(uuid).isPresent() ? new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR) : new ResponseEntity<>(HttpStatus.OK);
     }
-
 
 }
