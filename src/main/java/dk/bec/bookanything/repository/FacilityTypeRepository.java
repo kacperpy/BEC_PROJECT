ef488c56c9e8069109bd763713749c1c9bf3588b
kubagogolevski <jakub.gogolevski@gmail.com>
2022-02-28 11:18:12 +0100

    repositories added

--- src/main/java/dk/bec/bookanything/repository/FacilityTypeRepository.java
+++ src/main/java/dk/bec/bookanything/repository/FacilityTypeRepository.java
@@ -4,12 +4,9 @@ import dk.bec.bookanything.model.FacilityTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.UUID;
-
 @Repository
 public interface FacilityTypeRepository  extends JpaRepository<FacilityTypeEntity, Long> {
 
-    FacilityTypeEntity findFacilityTypeByUuid(UUID uuid);
+    FacilityTypeEntity findFacilityTypeById(Long id);
 
 }
