0173b89427f6cfea209ead062e15dec763be0fbe
JanRejnowski <jrx@bec.dk>
2022-02-28 10:27:09 +0100

    Added ReservationController;

---
+++ src/main/java/dk/bec/bookanything/controller/ReservationController.java
@@ -0,0 +1,26 @@
+package dk.bec.bookanything.controller;
+
+import dk.bec.bookanything.model.ReservationEntity;
+import dk.bec.bookanything.service.ReservationService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reservations")
+@AllArgsConstructor
+public class ReservationController {
+
+    @Autowired
+    private final ReservationService reservationService;
+
+    @GetMapping
+    public ResponseEntity<List<ReservationEntity>> getDiscountCodes() {
+        List<ReservationEntity> res = reservationService.getReservations();
+    }
+}
