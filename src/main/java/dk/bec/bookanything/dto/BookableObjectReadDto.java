198665545ba2117fb52216c25a8a95be0e960a7b
kacperpy <kacperpy@gmail.com>
2022-02-28 11:58:43 +0100

    bookable object PUT method, read&CREATE DTO created

---
+++ src/main/java/dk/bec/bookanything/dto/BookableObjectReadDto.java
@@ -0,0 +1,28 @@
+package dk.bec.bookanything.dto;
+
+import dk.bec.bookanything.model.FeatureEntity;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BookableObjectReadDto {
+
+    Long id;
+
+    String name;
+
+    Integer time_period;
+
+    Integer capacity;
+
+    String description;
+
+    LocalDateTime date_time;
+
+    Integer reservation_count;
+
+    FeatureEntity feature;
+}
--- src/main/java/dk/bec/bookanything/service/BookableObjectService.java
+++ src/main/java/dk/bec/bookanything/service/BookableObjectService.java
@@ -1,12 +1,14 @@
 package dk.bec.bookanything.service;
 
+import dk.bec.bookanything.dto.BookableObjectReadDto;
 import dk.bec.bookanything.model.BookableObjectEntity;
 import dk.bec.bookanything.repository.BookableObjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
-import java.util.UUID;
+import java.util.Optional;
 
 @Service
 public class BookableObjectService {
@@ -26,7 +28,26 @@ public class BookableObjectService {
         bookableObjectRepository.save(bookableObjectEntity);
     }
 
-    public BookableObjectEntity getBookableObjectByUUID(UUID uuid) {
-        return bookableObjectRepository.findByUuid(uuid);
+    public Optional<BookableObjectEntity> getBookableObjectById(Long id) {
+        return bookableObjectRepository.findById(id);
+    }
+
+    @Transactional
+    public BookableObjectReadDto updateBookableObject(BookableObjectEntity bookableObjectEntity) {
+        bookableObjectRepository.save(bookableObjectEntity);
+        return convertToReadDto(bookableObjectEntity);
+    }
+
+    private BookableObjectReadDto convertToReadDto(BookableObjectEntity bookableObjectEntity) {
+        return BookableObjectReadDto.builder()
+                .id(bookableObjectEntity.getId())
+                .name(bookableObjectEntity.getName())
+                .time_period(bookableObjectEntity.getTime_period())
+                .capacity(bookableObjectEntity.getCapacity())
+                .description(bookableObjectEntity.getDescription())
+                .date_time(bookableObjectEntity.getDate_time())
+                .reservation_count(bookableObjectEntity.getReservations() != null ? bookableObjectEntity.getReservations().size() : 0)
+                .feature(bookableObjectEntity.getFeature())
+                .build();
     }
 }
