a5d1567ec2b55be1bb00447b87a9c2bee5a5e1b4
JanRejnowski <jrx@bec.dk>
2022-03-01 14:40:44 +0100

    Added findByCity to AddressRepository;

--- src/main/java/dk/bec/bookanything/repository/AddressRepository.java
+++ src/main/java/dk/bec/bookanything/repository/AddressRepository.java
@@ -4,11 +4,10 @@ import dk.bec.bookanything.model.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
-
+    Optional<List<AddressEntity>> findByCity(String city);
 }
