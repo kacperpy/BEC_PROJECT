5d6437ad73acb15c644daabe36282b2030d16a67
m4teoosh <mateusz.kadlubowski99@gmail.com>
2022-03-02 13:29:18 +0100

    Added ErrorData, created handler for MethodArgumentNotValid Exception

---
+++ src/main/java/dk/bec/bookanything/exception/ErrorData.java
@@ -0,0 +1,20 @@
+package dk.bec.bookanything.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorData {
+
+    private String message;
+
+    private HttpStatus status;
+
+    private List<String> errors;
+}
--- src/main/java/dk/bec/bookanything/exception/GlobalExceptionHandler.java
+++ src/main/java/dk/bec/bookanything/exception/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.boot.context.properties.bind.validation.ValidationErr
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,10 +31,20 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ArrayList<>(e.getConstraintViolations());
     }
 
-
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        System.out.println(ex.getFieldErrors());
-        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+        List<String> errors = new ArrayList<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+
+        ErrorData errorData = new ErrorData("Validation error", status, errors);
+
+        return handleExceptionInternal(ex, errorData, headers, errorData.getStatus(), request);
     }
+
+
 }
