b51c44f48f244e37dc41d41260606cb2d1728237
m4teoosh <mateusz.kadlubowski99@gmail.com>
2022-03-03 15:01:39 +0100

    added methods to FutureDateTimeConstraint

--- src/main/java/dk/bec/bookanything/validator/FutureDateTimeConstraint.java
+++ src/main/java/dk/bec/bookanything/validator/FutureDateTimeConstraint.java
@@ -1,6 +1,7 @@
 package dk.bec.bookanything.validator;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
@@ -9,4 +10,8 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FutureDateTimeConstraint {
     String message() default "DateTime must be in the future";
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
 }
