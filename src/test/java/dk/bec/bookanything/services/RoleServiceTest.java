687014acf280bd6c59d273fd2bbc7a5a528dd42c
nuraiza <Nuraiza.MarufKyzy@bec.dk>
2022-03-03 16:22:00 +0100

    UserRoleTest is created and tested.

--- src/test/java/dk/bec/bookanything/services/RoleServiceTest.java
+++ src/test/java/dk/bec/bookanything/services/RoleServiceTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
-import java.time.LocalDate;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
